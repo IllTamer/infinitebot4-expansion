c0687e0ff941a6965818f32cfc5cad607c86fdd0
IllTamer <765743073@qq.com>
2022-08-09 23:45:54 +0800

    Refactor class structure

---
+++ chat-manager/src/main/java/com/illtamer/infinite/bot/expansion/chat/listener/GameMessageViewListener.java
@@ -0,0 +1,84 @@
+package com.illtamer.infinite.bot.expansion.chat.listener;
+
+import com.illtamer.infinite.bot.api.message.MessageBuilder;
+import com.illtamer.infinite.bot.expansion.chat.event.PreGame2GroupMessageEvent;
+import com.illtamer.infinite.bot.expansion.view.util.HologramUtil;
+import com.illtamer.infinite.bot.minecraft.Bootstrap;
+import com.illtamer.infinite.bot.minecraft.api.StaticAPI;
+import com.illtamer.infinite.bot.minecraft.expansion.ExpansionConfig;
+import com.loohp.interactivechat.InteractiveChat;
+import com.loohp.interactivechat.objectholders.ICPlayer;
+import com.loohp.interactivechat.objectholders.ICPlayerFactory;
+import com.loohp.interactivechatdiscordsrvaddon.api.InteractiveChatDiscordSrvAddonAPI;
+import com.loohp.interactivechatdiscordsrvaddon.graphics.ImageGeneration;
+import com.loohp.interactivechatdiscordsrvaddon.wrappers.GraphicsToPacketMapWrapper;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.UUID;
+
+public class GameMessageViewListener implements Listener {
+    public static GameMessageViewListener instance;
+    private final boolean expandChat;
+
+    public GameMessageViewListener(ExpansionConfig configFile) {
+        instance = this;
+        this.expandChat = configFile.getConfig().getBoolean("expand-chat") && StaticAPI.hasExpansion("ViewManager");
+    }
+
+    @EventHandler
+    public void onMessage(PreGame2GroupMessageEvent event) {
+        if (!expandChat || event.getCleanMessage().length() <= 2) return;
+        final Player player = event.getPlayer();
+        final ICPlayer icPlayer = ICPlayerFactory.getICPlayer(player);
+        String message = event.getCleanMessage();
+        boolean item = false, inv = false, end = false;
+
+        if (InteractiveChat.itemPlaceholder.matcher(message).find()) {
+            message = message.replaceAll(InteractiveChat.itemPlaceholder.pattern(), "");
+            item = true;
+        }
+        if (InteractiveChat.invPlaceholder.matcher(message).find()) {
+            message = message.replaceAll(InteractiveChat.invPlaceholder.pattern(), "");
+            inv = true;
+        }
+        if (InteractiveChat.enderPlaceholder.matcher(message).find()) {
+            message = message.replaceAll(InteractiveChat.enderPlaceholder.pattern(), "");
+            end = true;
+        }
+        final MessageBuilder builder = MessageBuilder.json()
+                .text(event.getPrefix())
+                .text(message);
+        try {
+            if (item) {
+                final ItemStack mainHand = player.getItemInHand();
+                builder.image("item", HologramUtil.imageToBase64(ImageGeneration.getItemStackImage(mainHand, icPlayer)))
+                        .image("tooltip", HologramUtil.imageToBase64(HologramUtil.getTooltip(mainHand, player)));
+            }
+            if (inv) builder.image("inv", HologramUtil.imageToBase64(ImageGeneration.getPlayerInventoryImage(player.getInventory(), icPlayer)));
+            if (end) builder.image("end", HologramUtil.imageToBase64(ImageGeneration.getInventoryImage(player.getEnderChest(), icPlayer)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        event.setMessage(builder.build());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onShowMapImage(AsyncPlayerChatEvent event) {
+        if (!expandChat) return;
+        final String message = event.getMessage();
+        if (!message.startsWith("vm-map#")) return;
+        final GraphicsToPacketMapWrapper wrapper = InteractiveChatDiscordSrvAddonAPI.
+                getDiscordImageWrapperByUUID(UUID.fromString(message.substring("vm-map#".length())));
+        Bukkit.getScheduler().runTask(Bootstrap.getInstance(), () -> {
+            wrapper.show(event.getPlayer());
+        });
+        event.setCancelled(true);
+    }
+
+}
