f16c69e5f23189e4222dbf6064ff4e609a767b89
IllTamer <765743073@qq.com>
2022-08-09 23:46:50 +0800

    Modify event trigger sequence

--- chat-manager/src/main/java/com/illtamer/infinite/bot/expansion/chat/listener/Game2GroupListener.java
+++ chat-manager/src/main/java/com/illtamer/infinite/bot/expansion/chat/listener/Game2GroupListener.java
@@ -1,29 +1,33 @@
 package com.illtamer.infinite.bot.expansion.chat.listener;
 
 import com.illtamer.infinite.bot.api.handler.OpenAPIHandling;
+import com.illtamer.infinite.bot.api.message.Message;
+import com.illtamer.infinite.bot.api.message.MessageBuilder;
 import com.illtamer.infinite.bot.expansion.chat.ChatManager;
 import com.illtamer.infinite.bot.expansion.chat.Global;
+import com.illtamer.infinite.bot.expansion.chat.event.PreGame2GroupMessageEvent;
 import com.illtamer.infinite.bot.minecraft.Bootstrap;
 import com.illtamer.infinite.bot.minecraft.expansion.ExpansionConfig;
 import com.illtamer.infinite.bot.minecraft.util.PluginUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Game2GroupListener implements Listener {
-
     private final boolean enable;
     private final String prefix;
     private final Map<String, Object> prefixMapper;
     private final Set<Long> allTurnsGroupIds;
+    private final Map<PreGame2GroupMessageEvent, AsyncPlayerChatEvent> eventMap = new HashMap<>();
 
     public Game2GroupListener(ExpansionConfig configFile) {
         ConfigurationSection section = configFile.getConfig().getConfigurationSection("game-to-group");
@@ -38,26 +42,40 @@ public class Game2GroupListener implements Listener {
                 .collect(Collectors.toSet());
     }
 
-    // 最后触发
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onChat(AsyncPlayerChatEvent event) {
+    // 较先触发(取消 InteractiveChat 消息)
+    @EventHandler(priority = EventPriority.LOW)
+    public void preChat(AsyncPlayerChatEvent event) {
         if (!enable || event.isCancelled()) return;
-        Bukkit.getScheduler().runTaskAsynchronously(Bootstrap.getInstance(), () -> {
-            final String message = event.getMessage();
-            final int index;
-            if (allTurnsGroupIds.size() != 0) {
-                final String format = format(event);
-                allTurnsGroupIds.forEach(messageConsumer(format));
-            }
-            if (message.length() <= 2 || (index = message.indexOf(' ')) == -1) return;
-            final String prefix = message.substring(0, index);
+        final Player player = event.getPlayer();
+        final String rawMessage = event.getMessage();
+
+        Set<Long> targetGroups = new HashSet<>(allTurnsGroupIds);
+        String cleanMessage = rawMessage;
+        final int index;
+        if (rawMessage.length() > 2 && (index = rawMessage.indexOf(' ')) != -1) {
+            final String prefix = rawMessage.substring(0, index);
             final Set<Long> turnsGroupIds = prefixMapper.entrySet().stream()
                     .filter(entry -> entry.getValue().equals(prefix))
                     .map(entry -> Long.parseLong(entry.getKey()))
                     .collect(Collectors.toSet());
-            final String format = format(event);
-            turnsGroupIds.forEach(messageConsumer(format));
-        });
+            targetGroups.addAll(turnsGroupIds);
+            cleanMessage = turnsGroupIds.size() == 0 ? rawMessage : rawMessage.substring(index+1);
+        }
+        PreGame2GroupMessageEvent messageEvent = new PreGame2GroupMessageEvent(Collections.unmodifiableSet(targetGroups), format(player), PluginUtil.clearColor(cleanMessage), player);
+        eventMap.put(messageEvent, event);
+        Bukkit.getPluginManager().callEvent(messageEvent);
+    }
+
+    // 最后触发
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onDispatch(PreGame2GroupMessageEvent messageEvent) {
+        final AsyncPlayerChatEvent event = eventMap.remove(messageEvent);
+        if (event == null) return;
+        Message message = messageEvent.getMessage() == null ?
+                MessageBuilder.json().text(messageEvent.getPrefix()).text(messageEvent.getCleanMessage()).build() :
+                messageEvent.getMessage();
+        Bukkit.getScheduler().runTaskAsynchronously(Bootstrap.getInstance(),
+                () -> messageEvent.getTargetGroups().forEach(Game2GroupListener.messageConsumer(message)));
     }
 
     // 最先触发
@@ -79,16 +97,16 @@ public class Game2GroupListener implements Listener {
         }
     }
 
-    private String format(AsyncPlayerChatEvent event) {
+    private String format(Player player) {
         return prefix
-                .replace("%player_name%", event.getPlayer().getName())
-                + event.getMessage();
+                .replace("%player_name%", player.getName());
     }
 
-    private static Consumer<Long> messageConsumer(String format) {
+    public static Consumer<Long> messageConsumer(Message message) {
         return groupId -> {
+            if (message == null) return;
             try {
-                OpenAPIHandling.sendGroupMessage(format, groupId);
+                OpenAPIHandling.sendGroupMessage(message, groupId);
             } catch (Exception e) {
                 e.printStackTrace();
             }
