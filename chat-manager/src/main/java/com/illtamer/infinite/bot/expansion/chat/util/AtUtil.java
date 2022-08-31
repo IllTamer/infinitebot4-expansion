7ca088fd426636c141ed1f8b388541a49c5a9f57
IllTamer <765743073@qq.com>
2022-08-31 15:23:47 +0800

    Update open @ sound configuration

--- chat-manager/src/main/java/com/illtamer/infinite/bot/expansion/chat/util/AtUtil.java
+++ chat-manager/src/main/java/com/illtamer/infinite/bot/expansion/chat/util/AtUtil.java
@@ -1,37 +1,31 @@
 package com.illtamer.infinite.bot.expansion.chat.util;
 
+import com.illtamer.infinite.bot.minecraft.expansion.ExpansionConfig;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 public class AtUtil {
 
+    private static Sound sound;
+
+    public static void init(ExpansionConfig config) {
+        sound = Sound.valueOf(config.getConfig().getString("group-to-game.at-sound"));
+    }
+
     public static void one(String title1, String title2, String message, Player target) {
         target.sendTitle(title1, title2);
         target.sendMessage(message);
-        playSound(1, target);
+        playSound(target);
     }
 
     public static void all(String title1, String title2, String message, Player target) {
         target.sendTitle(title1, title2);
         target.sendMessage(message);
-        playSound(1, target);
+        playSound(target);
     }
 
-    public static void playSound(int sound, Player target) {
-        switch(sound) {
-            case 1:
-                target.playSound(target.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
-                break;
-            case 2:
-                target.playSound(target.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 1.0F, 1.0F);
-                break;
-            case 3:
-                target.playSound(target.getLocation(), Sound.ENTITY_GHAST_SCREAM, 1.0F, 1.0F);
-                break;
-            case 4:
-                target.playSound(target.getLocation(), Sound.ENTITY_WITHER_DEATH, 1.0F, 1.0F);
-                break;
-        }
+    public static void playSound(Player target) {
+        target.playSound(target.getLocation(), sound, 1.0F, 1.0F);
     }
 
 }
