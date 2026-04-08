3c2574422e48925e4be2973a8c5e10c97c85ecd5
IllTamer <mail@illtamer.com>
2026-04-08 18:49:56 +0800

    initial commit

---
+++ message-manager/src/main/java/com/illtamer/infinite/bot/expansion/manager/message/MessageManager.java
@@ -0,0 +1,46 @@
+package com.illtamer.infinite.bot.expansion.manager.message;
+
+import com.illtamer.infinite.bot.expansion.manager.message.listener.GroupMessageListener;
+import com.illtamer.infinite.bot.minecraft.api.EventExecutor;
+import com.illtamer.infinite.bot.minecraft.expansion.ExpansionConfig;
+import com.illtamer.infinite.bot.minecraft.expansion.Language;
+import com.illtamer.infinite.bot.minecraft.expansion.manager.InfiniteExpansion;
+import lombok.Getter;
+
+@Getter
+public class MessageManager extends InfiniteExpansion {
+
+    @Getter
+    private static MessageManager instance;
+    private ExpansionConfig configFile;
+    private Language language;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        configFile = new ExpansionConfig("config.yml", this);
+        this.language = Language.of("language", this);
+        EventExecutor.registerEvents(new GroupMessageListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+    }
+
+    @Override
+    public String getExpansionName() {
+        return "MessageManager";
+    }
+
+    @Override
+    public String getVersion() {
+        return "2.0";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "IllTamer";
+    }
+
+}
---
+++ message-manager/src/main/java/com/illtamer/infinite/bot/expansion/manager/message/listener/GroupMessageListener.java
@@ -0,0 +1,14 @@
+package com.illtamer.infinite.bot.expansion.manager.message.listener;
+
+import com.illtamer.infinite.bot.minecraft.api.event.EventHandler;
+import com.illtamer.infinite.bot.minecraft.api.event.Listener;
+import com.illtamer.perpetua.sdk.event.message.GroupMessageEvent;
+
+public class GroupMessageListener implements Listener {
+
+    @EventHandler
+    public void onChat(GroupMessageEvent event) {
+
+    }
+
+}
